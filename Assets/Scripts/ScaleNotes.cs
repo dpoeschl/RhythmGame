6c3bfde029a41fdb382f20c78ff6842815421e1c
David Poeschl <dpoeschl@gmail.com>
2020-05-25 15:48:01 -0700

    Remove build warnings

--- Assets/Scripts/ScaleNotes.cs
+++ Assets/Scripts/ScaleNotes.cs
@@ -6,42 +6,42 @@ using UnityEngine;
 public class ScaleNotes : Singleton<ScaleNotes>
 {
     [SerializeField]
-    private AudioSource audioSource;
+    private AudioSource audioSource = null;
     public AudioSource AudioSource => audioSource;
 
     [SerializeField]
-    private AudioClip c6;
+    private AudioClip c6 = null;
     public AudioClip C6 => c6;
 
     [SerializeField]
-    private AudioClip d6;
+    private AudioClip d6 = null;
     public AudioClip D6 => d6;
 
     [SerializeField]
-    private AudioClip e6;
+    private AudioClip e6 = null;
     public AudioClip E6 => e6;
 
     [SerializeField]
-    private AudioClip f6;
+    private AudioClip f6 = null;
     public AudioClip F6 => f6;
 
     [SerializeField]
-    private AudioClip g6;
+    private AudioClip g6 = null;
     public AudioClip G6 => g6;
 
     [SerializeField]
-    private AudioClip a6;
+    private AudioClip a6 = null;
     public AudioClip A6 => a6;
 
     [SerializeField]
-    private AudioClip b6;
+    private AudioClip b6 = null;
     public AudioClip B6 => b6;
 
     [SerializeField]
-    private AudioClip c7;
+    private AudioClip c7 = null;
     public AudioClip C7 => c7;
 
     [SerializeField]
-    private AudioClip d7;
+    private AudioClip d7 = null;
     public AudioClip D7 => d7;
 }
