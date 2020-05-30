b8d0d97b5eac16d1cd294bfd1fceae59d0a67cae
David Poeschl <dpoeschl@gmail.com>
2020-05-30 16:44:25 -0700

    New gameplay board with better layout properties, columns light up

---
+++ Assets/Scripts/ColumnController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ColumnController : MonoBehaviour
+{
+    private Color defaultColor;
+
+    private void Awake()
+    {
+        defaultColor = GetComponent<Image>().color;
+    }
+
+    public void Default()
+    {
+        GetComponent<Image>().color = defaultColor;
+    }
+
+    public void Miss()
+    {
+        GetComponent<Image>().color = new Color(0, 0, 1);
+    }
+
+    public void Hit()
+    {
+        GetComponent<Image>().color = new Color(1, 1, 0);
+    }
+}
---
+++ Assets/Scripts/Gameplay2Controller.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class Gameplay2Controller : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject leftWhite = null;
+
+    [SerializeField]
+    private GameObject leftYellow = null;
+
+    [SerializeField]
+    private GameObject leftGreen = null;
+
+    [SerializeField]
+    private GameObject leftBlue = null;
+
+    [SerializeField]
+    private GameObject centerRed = null;
+
+    [SerializeField]
+    private GameObject rightBlue = null;
+
+    [SerializeField]
+    private GameObject rightGreen = null;
+
+    [SerializeField]
+    private GameObject rightYellow = null;
+
+    [SerializeField]
+    private GameObject rightWhite = null;
+
+    private (KeyCode, GameObject)[] keyCodes;
+
+    void Start()
+    {
+        keyCodes = new (KeyCode, GameObject)[]
+        {
+            (KeyCode.JoystickButton0, leftWhite),
+            (KeyCode.JoystickButton1, leftYellow),
+            (KeyCode.JoystickButton5, leftGreen),
+            (KeyCode.JoystickButton2, leftBlue),
+            (KeyCode.JoystickButton4, centerRed),
+            (KeyCode.JoystickButton3, rightBlue),
+            (KeyCode.JoystickButton7, rightGreen),
+            (KeyCode.Joystick8Button19, rightYellow), // HACK
+            (KeyCode.JoystickButton6, rightWhite),
+        };
+    }
+
+    void Update()
+    {
+        foreach (var (keyCode, column) in keyCodes)
+        {
+            bool pressed;
+            if (keyCode == KeyCode.Joystick8Button19)
+            {
+                pressed = Input.GetAxis("Vertical") == 1;
+            }
+            else
+            {
+                pressed = Input.GetKey(keyCode);
+            }
+
+            var columnController = column.GetComponent<ColumnController>();
+            if (pressed)
+            {
+                columnController.Miss();
+            }
+            else
+            {
+                columnController.Default();
+            }
+        }
+    }
+}
