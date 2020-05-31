dd34deb774e8e29f97b05d594a3174af0ce1a0ba
David Poeschl <dpoeschl@gmail.com>
2020-05-30 18:32:08 -0700

    Create notes that move correctly, no hit detection yet.

--- Assets/Scripts/Gameplay2Controller.cs
+++ Assets/Scripts/Gameplay2Controller.cs
@@ -2,9 +2,44 @@
 using System.Collections;
 using System;
 using UnityEngine.UI;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 public class Gameplay2Controller : MonoBehaviour
 {
+    (int, int)[] song = new (int, int)[]
+    {
+        (0, 0),
+        (500, 1),
+        (1000, 2),
+        (1500, 3),
+        (2000, 4),
+        (2500, 5),
+        (3000, 6),
+        (3500, 7),
+        (4000, 8),
+        (4250, 7),
+        (4500, 6),
+        (4750, 5),
+        (5000, 4),
+        (5250, 3),
+        (5500, 2),
+        (5750, 1),
+        (6000, 0),
+        (7000, 0),
+        (7000, 2),
+        (8000, 1),
+        (8000, 3),
+        (9000, 2),
+        (9000, 4),
+        (10000, 1),
+        (10000, 3),
+        (11000, 0),
+        (11000, 2),
+    };
+
+    List<(GameObject, int)> notes = new List<(GameObject, int)>();
+
     [SerializeField]
     private GameObject leftWhite = null;
 
@@ -35,6 +70,8 @@ public class Gameplay2Controller : MonoBehaviour
     private (KeyCode, GameObject, AudioClip)[] keyCodes;
     private bool[] previouslyHit = new bool[9];
 
+    Stopwatch stopwatch;
+
     void Start()
     {
         keyCodes = new (KeyCode, GameObject, AudioClip)[]
@@ -49,9 +86,20 @@ public class Gameplay2Controller : MonoBehaviour
             (KeyCode.Joystick8Button19, rightYellow, ScaleNotes.Instance.C7), // HACK
             (KeyCode.JoystickButton6, rightWhite, ScaleNotes.Instance.D7)
         };
+
+        CreateAllNotes();
+
+        stopwatch = Stopwatch.StartNew();
     }
 
     void Update()
+    {
+        MoveNotes();
+
+        ProcessButtons();
+    }
+
+    private void ProcessButtons()
     {
         for (int i = 0; i < 9; i++)
         {
@@ -83,6 +131,89 @@ public class Gameplay2Controller : MonoBehaviour
             }
 
             previouslyHit[i] = pressed;
+        }
+    }
+
+    private void CreateAllNotes()
+    {
+        foreach (var (time, column) in song)
+        {
+            GameObject parent = null;
+            Color color = default;
+            switch(column)
+            {
+                case 0:
+                    parent = leftWhite;
+                    color = Color.white;
+                    break;
+                case 1:
+                    parent = leftYellow;
+                    color = Color.yellow;
+                    break;
+                case 2:
+                    parent = leftGreen;
+                    color = Color.green;
+                    break;
+                case 3:
+                    parent = leftBlue;
+                    color = Color.blue;
+                    break;
+                case 4:
+                    parent = centerRed;
+                    color = Color.red;
+                    break;
+                case 5:
+                    parent = rightBlue;
+                    color = Color.blue;
+                    break;
+                case 6:
+                    parent = rightGreen;
+                    color = Color.green;
+                    break;
+                case 7:
+                    parent = rightYellow;
+                    color = Color.yellow;
+                    break;
+                case 8:
+                    parent = rightWhite;
+                    color = Color.white;
+                    break;
+            }
+
+            switch (column)
+            {
+                default:
+                    GameObject panel = new GameObject("Panel");
+                    panel.AddComponent<CanvasRenderer>();
+
+                    RectTransform rectTransform = panel.AddComponent<RectTransform>();
+                    rectTransform.anchorMin = new Vector2(0, 0.09f + time/2000f);
+                    rectTransform.anchorMax = new Vector2(1, 0.11f + time/2000f);
+
+                    rectTransform.offsetMin = Vector2.zero;
+                    rectTransform.offsetMax = Vector2.zero;
+
+                    Image i = panel.AddComponent<Image>();
+                    i.color = color;
+
+                    panel.transform.SetParent(parent.transform, false);
+
+                    notes.Add((panel, time));
+
+                    break;
+            }
+        }
+    }
+
+    private void MoveNotes()
+    {
+        var millis = stopwatch.ElapsedMilliseconds;
+
+        foreach (var (gameObject, time) in notes)
+        {
+            RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+            rectTransform.anchorMin = new Vector2(0,0.09f + (time - millis) / 2000f);
+            rectTransform.anchorMax = new Vector2(1,0.11f + (time - millis) / 2000f);
         }
     }
 }
