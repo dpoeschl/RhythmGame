634c7e754d11eb028000e36de74cb61cff7022bd
David Poeschl <dpoeschl@gmail.com>
2020-05-30 17:00:12 -0700

    Pressing buttons makes sounds

--- Assets/Scripts/Gameplay2Controller.cs
+++ Assets/Scripts/Gameplay2Controller.cs
@@ -32,28 +32,31 @@ public class Gameplay2Controller : MonoBehaviour
     [SerializeField]
     private GameObject rightWhite = null;
 
-    private (KeyCode, GameObject)[] keyCodes;
+    private (KeyCode, GameObject, AudioClip)[] keyCodes;
+    private bool[] previouslyHit = new bool[9];
 
     void Start()
     {
-        keyCodes = new (KeyCode, GameObject)[]
+        keyCodes = new (KeyCode, GameObject, AudioClip)[]
         {
-            (KeyCode.JoystickButton0, leftWhite),
-            (KeyCode.JoystickButton1, leftYellow),
-            (KeyCode.JoystickButton5, leftGreen),
-            (KeyCode.JoystickButton2, leftBlue),
-            (KeyCode.JoystickButton4, centerRed),
-            (KeyCode.JoystickButton3, rightBlue),
-            (KeyCode.JoystickButton7, rightGreen),
-            (KeyCode.Joystick8Button19, rightYellow), // HACK
-            (KeyCode.JoystickButton6, rightWhite),
+            (KeyCode.JoystickButton0, leftWhite, ScaleNotes.Instance.C6),
+            (KeyCode.JoystickButton1, leftYellow, ScaleNotes.Instance.D6),
+            (KeyCode.JoystickButton5, leftGreen, ScaleNotes.Instance.E6),
+            (KeyCode.JoystickButton2, leftBlue, ScaleNotes.Instance.F6),
+            (KeyCode.JoystickButton4, centerRed, ScaleNotes.Instance.G6),
+            (KeyCode.JoystickButton3, rightBlue, ScaleNotes.Instance.A6),
+            (KeyCode.JoystickButton7, rightGreen, ScaleNotes.Instance.B6),
+            (KeyCode.Joystick8Button19, rightYellow, ScaleNotes.Instance.C7), // HACK
+            (KeyCode.JoystickButton6, rightWhite, ScaleNotes.Instance.D7)
         };
     }
 
     void Update()
     {
-        foreach (var (keyCode, column) in keyCodes)
+        for (int i = 0; i < 9; i++)
         {
+            var (keyCode, column, clip) = keyCodes[i];
+
             bool pressed;
             if (keyCode == KeyCode.Joystick8Button19)
             {
@@ -67,12 +70,19 @@ public class Gameplay2Controller : MonoBehaviour
             var columnController = column.GetComponent<ColumnController>();
             if (pressed)
             {
-                columnController.Miss();
+                columnController.Hit();
             }
             else
             {
                 columnController.Default();
             }
+
+            if (pressed && !previouslyHit[i])
+            {
+                ScaleNotes.Instance.AudioSource.PlayOneShot(clip);
+            }
+
+            previouslyHit[i] = pressed;
         }
     }
 }
