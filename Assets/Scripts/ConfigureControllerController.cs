6e2d3aed2c0f80cfeff6f25bb2d4ceb02d13ce98
David Poeschl <dpoeschl@gmail.com>
2020-05-31 09:16:52 -0700

    ConfigureController now shows which buttons are being pressed

--- Assets/Scripts/ConfigureControllerController.cs
+++ Assets/Scripts/ConfigureControllerController.cs
@@ -1,14 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConfigureControllerController : MonoBehaviour
 {
     [SerializeField]
     private ScreenController screenController = null;
 
+    [SerializeField]
+    private Text currentInputsText = null;
+
     public void OnBackButtonClicked()
     {
         screenController.ShowScreen(GameScreen.MainMenu);
+    }
+
+    private void Update()
+    {
+        string result = string.Empty;
+
+        foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKey(keyCode))
+            {
+                result += $"{keyCode.ToString()};";
+            }
+        }
+
+        currentInputsText.text = result;
     }
 }
